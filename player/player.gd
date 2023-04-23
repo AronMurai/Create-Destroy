3545d154ef7d76f8a43566a4b616443b2fbbd7ed
AronEiichi <muraiaron@gmail.com>
2023-04-23 11:36:14 -0700

    decoration complete

--- player/player.gd
+++ player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 
 const MOUSE_SENSITIVITY = 0.1
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
